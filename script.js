8fc1ed1dd9aff70312413b9aab6a0834267dca93
ruhul-islam <islamruhul48092@gmail.com>
2022-07-08 15:14:08 -0400

    after Mitch helped

--- script.js
+++ script.js
@@ -78,23 +78,14 @@ const dessertsContainer = document.querySelector(".desserts");
 dessertsContainer.classList.add("menu-container");
 const beveragesContainer = document.querySelector(".beverages");
 beveragesContainer.classList.add("menu-container");
+const mainContainer = document.querySelector("main");
 
-const plusContainer = document.querySelector("fa-circle-plus");
-// const cartContainer = document.querySelector(".nav-list");
-// const createCart = () => {
-//   const newItem = document.createElement("a");
-//   const cart = document.createElement("i");
-//   cart.classList.add("fa-solid", "fa-cart-shopping");
-//   newItem.append(cart);
-//   cartContainer.append(newItem);
-// };
-// createCart();
-// //<i class="fa-light fa-cart-shopping"></i>
+const cart = [];
 
-const createAppetizerItems = () => {
-  appetizers.forEach((item) => {
-    const newAppetizer = document.createElement("li");
-    newAppetizer.classList.add("appetizer");
+const createItems = (container, array, category) => {
+  array.forEach((item, i) => {
+    const newItem = document.createElement("li");
+    newItem.classList.add(category);
 
     const itemNameParagraph = document.createElement("p");
     const itemPriceParagraph = document.createElement("p");
@@ -105,92 +96,37 @@ const createAppetizerItems = () => {
     itemImgParagraph.classList.add("image");
     const cartPlus = document.createElement("i");
     cartPlus.classList.add("fa-solid", "fa-circle-plus");
-    newAppetizer.append(
+    cartPlus.setAttribute("data-index", i);
+    cartPlus.setAttribute("data-category", category);
+
+    newItem.append(
       itemImgParagraph,
       itemNameParagraph,
       itemPriceParagraph,
       cartPlus
     );
-    appetizersContainer.append(newAppetizer);
-    // appetizersContainer.append(cartPlus);
+    newItem.setAttribute("data-index", i);
+    container.append(newItem);
   });
 };
-createAppetizerItems();
+createItems(appetizersContainer, appetizers, "appetizer");
+createItems(entreesContainer, entrees, "entree");
+createItems(dessertsContainer, desserts, "dessert");
+createItems(beveragesContainer, beverages, "beverage");
 
-const createEntreeItems = () => {
-  entrees.forEach((item) => {
-    const newEntree = document.createElement("li");
-    newEntree.classList.add("entree");
-
-    const itemNameParagraph = document.createElement("p");
-    const itemPriceParagraph = document.createElement("p");
-    const itemImgParagraph = document.createElement("img");
-    itemNameParagraph.textContent = item.itemName;
-    itemPriceParagraph.textContent = item.itemPrice;
-    itemImgParagraph.setAttribute("src", item.itemImg);
-    itemImgParagraph.classList.add("image");
-    const cartPlus = document.createElement("i");
-    cartPlus.classList.add("fa-solid", "fa-circle-plus");
-    newEntree.append(
-      itemImgParagraph,
-      itemNameParagraph,
-      itemPriceParagraph,
-      cartPlus
-    );
-    entreesContainer.append(newEntree);
-  });
-};
-createEntreeItems();
-
-const createDessertItems = () => {
-  desserts.forEach((item) => {
-    const newDessert = document.createElement("li");
-    newDessert.classList.add("dessert");
-
-    const itemNameParagraph = document.createElement("p");
-    const itemPriceParagraph = document.createElement("p");
-    const itemImgParagraph = document.createElement("img");
-    itemNameParagraph.textContent = item.itemName;
-    itemPriceParagraph.textContent = item.itemPrice;
-    itemImgParagraph.setAttribute("src", item.itemImg);
-    itemImgParagraph.classList.add("image");
-    const cartPlus = document.createElement("i");
-    cartPlus.classList.add("fa-solid", "fa-circle-plus");
-    newDessert.append(
-      itemImgParagraph,
-      itemNameParagraph,
-      itemPriceParagraph,
-      cartPlus
-    );
-    dessertsContainer.append(newDessert);
-  });
-};
-createDessertItems();
-
-const createBeverageItems = () => {
-  beverages.forEach((item) => {
-    const newBeverage = document.createElement("li");
-    newBeverage.classList.add("beverage");
-
-    const itemNameParagraph = document.createElement("p");
-    const itemPriceParagraph = document.createElement("p");
-    const itemImgParagraph = document.createElement("img");
-    itemNameParagraph.textContent = item.itemName;
-    itemPriceParagraph.textContent = item.itemPrice;
-    itemImgParagraph.setAttribute("src", item.itemImg);
-    itemImgParagraph.classList.add("image");
-    const cartPlus = document.createElement("i");
-    cartPlus.classList.add("fa-solid", "fa-circle-plus");
-    newBeverage.append(
-      itemImgParagraph,
-      itemNameParagraph,
-      itemPriceParagraph,
-      cartPlus
-    );
-
-    beveragesContainer.append(newBeverage);
-  });
-};
-createBeverageItems();
-
-plusContainer.addEventListener("click", (e) => {});
+mainContainer.addEventListener("click", (e) => {
+  if (e.target.getAttribute("data-category") === "appetizer") {
+    const index = e.target.getAttribute("data-index");
+    cart.push(appetizers[index]);
+  } else if (e.target.getAttribute("data-category") === "entree") {
+    const index = e.target.getAttribute("data-index");
+    cart.push(entrees[index]);
+  } else if (e.target.getAttribute("data-category") === "dessert") {
+    const index = e.target.getAttribute("data-index");
+    cart.push(desserts[index]);
+  } else if (e.target.getAttribute("data-category") === "beverage") {
+    const index = e.target.getAttribute("data-index");
+    cart.push(beverages[index]);
+  }
+  console.log(cart);
+});
