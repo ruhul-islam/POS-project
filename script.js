6063bab202e46e30bd43a2b872051581d410b25a
ruhul-islam <islamruhul48092@gmail.com>
2022-07-08 11:13:59 -0400

    nothing

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const appetizers = [
     itemImg: "assets/hummus-pita.jpg",
     itemName: "Hummus & Pita",
     itemPrice: 6.5,
+    count: 1,
   },
   {
     itemImg: "assets/charcuterie.jpg",
     itemName: "Charcuterie Board",
     itemPrice: 11.5,
+    count: 1,
   },
 ];
 const entrees = [
@@ -17,16 +19,19 @@ const entrees = [
     itemImg: "assets/lamb-chops.jpg",
     itemName: "Lamb Chops",
     itemPrice: 22.5,
+    count: 1,
   },
   {
     itemImg: "assets/new-york-strip.jpg",
     itemName: "New York Strip",
     itemPrice: 30.5,
+    count: 1,
   },
   {
     itemImg: "assets/salmon.jpg",
     itemName: "Salmon",
     itemPrice: 19.5,
+    count: 1,
   },
 ];
 const desserts = [
@@ -34,16 +39,19 @@ const desserts = [
     itemImg: "assets/baklava.jpg",
     itemName: "Baklava",
     itemPrice: 5,
+    count: 1,
   },
   {
     itemImg: "assets/hot-fudge-brownie.jpg",
     itemName: "Hot Fudge Brownie",
     itemPrice: 6.5,
+    count: 1,
   },
   {
     itemImg: "assets/cheesecake.jpg",
     itemName: "Cheesecake",
     itemPrice: 8,
+    count: 1,
   },
 ];
 
@@ -52,11 +60,13 @@ const beverages = [
     itemImg: "assets/water-bottle.jpg",
     itemName: "Bottled Water",
     itemPrice: 1.5,
+    count: 1,
   },
   {
     itemImg: "assets/bottled-beer.jpg",
     itemName: "Bottled Beer",
     itemPrice: 4,
+    count: 1,
   },
 ];
 
@@ -68,7 +78,9 @@ const dessertsContainer = document.querySelector(".desserts");
 dessertsContainer.classList.add("menu-container");
 const beveragesContainer = document.querySelector(".beverages");
 beveragesContainer.classList.add("menu-container");
-const cartContainer = document.querySelector(".nav-list");
+
+const plusContainer = document.querySelector("fa-circle-plus");
+// const cartContainer = document.querySelector(".nav-list");
 // const createCart = () => {
 //   const newItem = document.createElement("a");
 //   const cart = document.createElement("i");
@@ -91,12 +103,16 @@ const createAppetizerItems = () => {
     itemPriceParagraph.textContent = item.itemPrice;
     itemImgParagraph.setAttribute("src", item.itemImg);
     itemImgParagraph.classList.add("image");
+    const cartPlus = document.createElement("i");
+    cartPlus.classList.add("fa-solid", "fa-circle-plus");
     newAppetizer.append(
       itemImgParagraph,
       itemNameParagraph,
-      itemPriceParagraph
+      itemPriceParagraph,
+      cartPlus
     );
     appetizersContainer.append(newAppetizer);
+    // appetizersContainer.append(cartPlus);
   });
 };
 createAppetizerItems();
@@ -113,7 +129,14 @@ const createEntreeItems = () => {
     itemPriceParagraph.textContent = item.itemPrice;
     itemImgParagraph.setAttribute("src", item.itemImg);
     itemImgParagraph.classList.add("image");
-    newEntree.append(itemImgParagraph, itemNameParagraph, itemPriceParagraph);
+    const cartPlus = document.createElement("i");
+    cartPlus.classList.add("fa-solid", "fa-circle-plus");
+    newEntree.append(
+      itemImgParagraph,
+      itemNameParagraph,
+      itemPriceParagraph,
+      cartPlus
+    );
     entreesContainer.append(newEntree);
   });
 };
@@ -131,7 +154,14 @@ const createDessertItems = () => {
     itemPriceParagraph.textContent = item.itemPrice;
     itemImgParagraph.setAttribute("src", item.itemImg);
     itemImgParagraph.classList.add("image");
-    newDessert.append(itemImgParagraph, itemNameParagraph, itemPriceParagraph);
+    const cartPlus = document.createElement("i");
+    cartPlus.classList.add("fa-solid", "fa-circle-plus");
+    newDessert.append(
+      itemImgParagraph,
+      itemNameParagraph,
+      itemPriceParagraph,
+      cartPlus
+    );
     dessertsContainer.append(newDessert);
   });
 };
@@ -149,8 +179,18 @@ const createBeverageItems = () => {
     itemPriceParagraph.textContent = item.itemPrice;
     itemImgParagraph.setAttribute("src", item.itemImg);
     itemImgParagraph.classList.add("image");
-    newBeverage.append(itemImgParagraph, itemNameParagraph, itemPriceParagraph);
+    const cartPlus = document.createElement("i");
+    cartPlus.classList.add("fa-solid", "fa-circle-plus");
+    newBeverage.append(
+      itemImgParagraph,
+      itemNameParagraph,
+      itemPriceParagraph,
+      cartPlus
+    );
+
     beveragesContainer.append(newBeverage);
   });
 };
 createBeverageItems();
+
+plusContainer.addEventListener("click", (e) => {});
